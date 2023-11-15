ac7f67f687b7f6b8b8ebb847009afc3c567ace54
zawhtetaung211 <zawhtetaung21199@gmail.com>
2023-11-15 19:33:07 +0630

    Initial Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <div className="text-white text-3xl font-bold">Zaw Htet Aung <span className="text-gray-500 font-thin">Portfolio</span></div>
+      <div className="text-gray-500 text-2xl mt-10 animate-bounce">Coming Soon...</div>
+      
+    </main>
+  );
+}
