ac4020492d35638cb3207174ba65cd202b413df6
dev-zha <zawhtetaung21199@gmail.com>
2024-07-10 18:07:11 +0630

    Fix Temporarily Env Variable

--- src/components/shared/GoogleAnalytics.tsx
+++ src/components/shared/GoogleAnalytics.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { GoogleAnalytics as GoogleAnalyticsFromLib } from '@next/third-parties/google';
 
 export default function GoogleAnalytics() {
-  const measurementId = process.env.NEXT_PUBLIC_MEASUREMENT_ID;
+  const measurementId = process.env.NEXT_PUBLIC_MEASUREMENT_ID || "G-DG56DWKNWN";
 
   if (!measurementId) {
     return null;
